3500f1a70e57081dc51c284a57b56039176c6b59
Chetan <chetandhembre@gmail.com>
2015-07-17 01:52:19 +0530

    fix limit bug
    now we do not check whether number can represent in 32 bit buffer as it will be handle by native buffer

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ var dz = require('dezalgo')
 
 function toUInt32 (n, callback) {
   callback = dz(callback)
-
-  if (n >= 0xff) {
-    return callback(new Error('Number can not represent in 32 bit'))
-  }
-
   var buffr = new Buffer(4)
   buffr.writeUInt32BE(n, 0)
   return callback(null, buffr)
 }
 
 function toUInt32Sync (n) {
-  if (n >= 0xff) {
-    throw new Error('Number can not represent in 32 bit')
-  }
   var buffr = new Buffer(4)
   buffr.writeUInt32BE(n, 0)
   return buffr
--- test/test.js
+++ test/test.js
@@ -1,15 +1,6 @@
 var tap = require('tap')
 var toUInt32 = require('../')
 
-tap.test('should return error when number can not fit in 32 bit', function (t) {
-  toUInt32(0xfff, function (err, buff) {
-    t.type(err, Error)
-    t.equal(err.message, 'Number can not represent in 32 bit', 'handling error for number can not fit in 32 bit')
-    t.notOk(buff, 'buff should be null')
-    t.end()
-  })
-})
-
 tap.test('should return corrent buffer for value number', function (t) {
   toUInt32(0xC, function (err, buffr) {
     t.notOk(err, 'err should not present')
@@ -18,13 +9,6 @@ tap.test('should return corrent buffer for value number', function (t) {
     t.equal(buffr.readUInt32BE(0), 0xC)
     t.end()
   })
-})
-
-tap.test('toUInt32Sync: should throw error when number can not fit in 32 bit', function (t) {
-  t.throws(function () {
-    toUInt32.toUInt32Sync(0xfff)
-  }, 'Number can not represent in 32 bit')
-  t.end()
 })
 
 tap.test('toUInt32Sync: should return correct buffer for value number', function (t) {
